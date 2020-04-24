2f8d44c0f363bd3363c05b6188f7a6d9b1dbe597
curliph <curliph@gmail.com>
2020-04-24 16:57:07 +0800

    add lgt8f328p support

--- swd_drv.h
+++ swd_drv.h
@@ -8,6 +8,10 @@
 extern "C"{
 #endif
 
+#define LGTMCU_8F328D		0x1
+#define LGTMCU_8F328P		0x2
+#define LGTMCU_UNKNOWN		0x0
+
 #define SWDIF_PIN		PINB
 #define SWDIF_DIR		DDRB
 #define SWDIF_PORT		PORTB
@@ -53,6 +57,9 @@ void SWD_SWDEN();
 uint8_t SWD_UnLock();
 void SWD_EEE_Write(uint16_t, uint16_t);
 uint16_t SWD_EEE_Read(uint16_t);
+
+void SWD_EEP_Write(uint32_t, uint16_t);
+uint32_t SWD_EEP_Read(uint16_t);
 
 #ifdef __cplusplus
 }
